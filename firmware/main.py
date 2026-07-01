2f81dad35dd5a5a99fcb98780c9045ad034e10ab
nickBaxter08 <nicholas.bax88@gmail.com>
2026-07-01 09:54:24 -0700

    Add files via upload

---
+++ firmware/main.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+
+print("================")
+print(" NOMAD NODE")
+print("================")
+
+sleep(1)
+
+print("Initializing...")
+sleep(1)
+
+print("WiFi: WAITING")
+sleep(1)
+
+print("Sensors: OFFLINE")
+sleep(1)
+
+print("STATUS: READY")
